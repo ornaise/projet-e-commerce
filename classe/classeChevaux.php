1b093ff1bd8c454549584b520611c4d255519730
Alexandra <zayaza28.ac@gmail.com>
2022-02-25 13:22:58 +0100

    ajout des function pour cheval  classe

--- classe/classeChevaux.php
+++ classe/classeChevaux.php
@@ -1,19 +1,28 @@
 <?php
 class cheval//classe qui concerne les chevaux
 {
+    public $Nom_Cheval;
     public $age;
     public $sexe;
     public $poids;
     public $race;
     public $taille;
 
-    public function __construct($age, $sexe, $poids, $race, $taille)
+
+
+    public function __construct($Nom_Cheval,$age, $sexe, $poids, $race, $taille)
     {
+        return $this->Nom_Cheval = $Nom_Cheval;
         return $this->age = $age;
         return  $this->sexe = $sexe;
         return $this->poids = $poids;
         return $this->race = $race;
         return $this->taille = $taille;
     }
+    
 }
+    class AjoutCheval extends cheval {
+            
+    }
+$Nom_Cheval = new cheval($Nom_Cheval,$age,$sexe,$poids,$race,$taille);
 ?>
